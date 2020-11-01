c56a76410c2c9fc4d8059a62750587d0af6b2051
ryo ariyama <ariyama@sansan.com>
2020-11-02 02:42:52 +0900

    check whether the number of arguments is correct or not

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,5 @@ RUN DEBIAN_FRONTEND=noninteractive apt install -y gcc make git binutils libc6-de
 RUN adduser --disabled-password --gecos '' user
 RUN echo 'user ALL=(root) NOPASSWD:ALL' > /etc/sudoers.d/user
 
-RUN curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-
-
 USER user
 WORKDIR /home/user
--- src/main.go
+++ src/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("the number of arguments is not correct")
+		return
+	}
 	fmt.Println(".intel_syntax noprefix\n")
 	fmt.Println(".globl main\n")
 	fmt.Println("main:\n")
-	flag.Parse()
-	fmt.Printf("  mov rax, %v\n", flag.Arg(0))
+	fmt.Printf("  mov rax, %v\n", os.Args[1])
 	fmt.Println("  ret\n")
+	return
 }
